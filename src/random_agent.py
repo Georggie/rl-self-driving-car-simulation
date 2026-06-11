0d699e1c67b7dfa641b3f0fa93dd7cceeea71268
George El-khoury <georgeel-khoury@my.unt.edu>
2026-06-11 13:15:27 -0500

    Add initaial random agent implemetation

---
+++ src/random_agent.py
@@ -0,0 +1,15 @@
+"""
+RL Self-Driving Car Simulation
+
+Phase 1:
+Random baseline agent.
+"""
+
+
+def main():
+    print("Random Agent Placeholder")
+
+
+    
+if __name__ == "__main__":
+    main()
